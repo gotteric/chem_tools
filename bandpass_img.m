5ad895db2735ada76150778630eadf5088838aae
Eric Gottlieb <gott.eric@gmail.com>
2013-09-11 15:07:58 -0400

    Done with debugging to run, onto debugging to make it do what I want...

--- bandpass_img.m
+++ bandpass_img.m
@@ -1,11 +1,12 @@
 function out = bandpass_img( img, sigX, sigY, x0, y0 )
 
-    imgFft = fftshift(fft2( img );
+    imgFft = fftshift(fft2( img ));
     filterMat = bandpass( size(img), sigX, sigY, x0, y0 );
-    
+    filtered = imgFft .* filterMat;
+    out = ifft2( ifftshift(filtered) );
 end
 
-function fitlerMat = bandpass( imgSize, sigX, sigY, x0, y0 )
+function filterMat = bandpass( imgSize, sigX, sigY, x0, y0 )
     % A table of bandpass values are made by using the error
     % function with x and y shifts, and standard deviations
     % of sigX and sigY
@@ -18,7 +19,7 @@ function fitlerMat = bandpass( imgSize, sigX, sigY, x0, y0 )
 
     [sigX, sigY, x0, y0] = checkInputs( sigX, sigY, x0, y0 );
     xAxis = ones(imgSize(1));
-    yAxis = ones(imageSize(2));
+    yAxis = ones(imgSize(2));
 
     xHighPass = pass( xAxis, sigX(1), x0(1), 0 );
     xLowPass = pass( xAxis, sigX(2), x0(2), 1 );
@@ -33,23 +34,23 @@ end
 
 function val = pass( x, sigX, x0, isLow )
 
-    erfArg = (-1)^isLow * (x-x0)/(sigX * 2^0.5)
-    val = (erf( erfArg ) + 1)/2
+    erfArg = (-1)^isLow * (x-x0)/(sigX * 2^0.5);
+    val = (erf( erfArg ) + 1)/2;
 end
 
 function [sigX, sigY, x0, y0] = checkInputs( sigX, sigY, x0, y0 )
 
     if length( sigX ) == 1
-        sigX(2) = sigX(1)
+        sigX(2) = sigX(1);
     end
     if length( sigX ) == 1
-        sigX(2) = sigX(1)
+        sigX(2) = sigX(1);
     end
     if length( sigX ) == 1
-        sigX(2) = sigX(1)
+        sigX(2) = sigX(1);
     end
     if length( sigX ) == 1
-        sigX(2) = sigX(1)
+        sigX(2) = sigX(1);
     end
 
 end
