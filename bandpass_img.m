f98b8bca07ae9eccf03f9684d05fb230a4ca5fa1
Eric Gottlieb <gott.eric@gmail.com>
2013-09-18 12:15:36 -0400

    bandpass_img is fully working, bandpass_ui is almost fully working

--- bandpass_img.m
+++ bandpass_img.m
@@ -1,56 +1,38 @@
-function out = bandpass_img( img, sigX, sigY, x0, y0 )
+function out = bandpass_img( img, sig1, sig2 )
+    % img is a 2d matrix with scalar value
+    
+    flat = poly_flatten( img, 3 );
 
-    imgFft = fftshift(fft2( img ));
-    filterMat = bandpass( size(img), sigX, sigY, x0, y0 );
+    dim = size( img );
+    nx = dim(1);
+    ny = dim(2);
+
+    imgFft = fftshift(fft2( flat, 2*nx-1, 2*ny-1 ));
+
+    filterMat = bandpass( img, sig1, sig2 );
+
     filtered = imgFft .* filterMat;
-    out = ifft2( ifftshift(filtered) );
+    out = ifft2(ifftshift(filtered));
+    out = real(out(1:nx, 1:ny));
 end
 
-function filterMat = bandpass( imgSize, sigX, sigY, x0, y0 )
-    % A table of bandpass values are made by using the error
-    % function with x and y shifts, and standard deviations
-    % of sigX and sigY
-    % vals of sig and x0/y0 must be stored as such:
-    %   x0 = (high, low)
-    % where high and low are x0 for their respective pass filters
-    %
-    % sig and x0/y0 can also be stand alone vals
-    % Then treats high and low vals as the same
+function filter3 = bandpass( img, sig1, sig2 )
 
-    [sigX, sigY, x0, y0] = checkInputs( sigX, sigY, x0, y0 );
-    xAxis = ones(imgSize(1));
-    yAxis = ones(imgSize(2));
+    dim = size( img );
+    nx = dim(1);
+    ny = dim(2);
 
-    xHighPass = pass( xAxis, sigX(1), x0(1), 0 );
-    xLowPass = pass( xAxis, sigX(2), x0(2), 1 );
-    xBand = xHighPass .* xLowPass;
+    filter1 = ones(2*nx-1,2*ny-1);
+    filter2 = ones(2*nx-1,2*ny-1);
+    filter3 = ones(2*nx-1,2*ny-1);
 
-    yHighPass = pass( yAxis, sigY(1), y0(1), 0 );
-    yLowPass = pass( yAxis, sigY(2), y0(2), 1 );
-    yBand = yHighPass .* yLowPass;
+    for i = 1:2*nx-1
+        for j = 1:2*ny-1
+            dist = ((i - (nx + 1))^2 + (j - (ny + 1))^2)^.5;
 
-    filterMat = bsxfun( @times, xBand', yBand );
-end
-
-function val = pass( x, sigX, x0, isLow )
-
-    erfArg = (-1)^isLow * (x-x0)/(sigX * 2^0.5);
-    val = (erf( erfArg ) + 1)/2;
-end
-
-function [sigX, sigY, x0, y0] = checkInputs( sigX, sigY, x0, y0 )
-
-    if length( sigX ) == 1
-        sigX(2) = sigX(1);
+            filter1(i,j) = exp(-dist^2/(2*sig1^2));
+            filter2(i,j) = 1 - exp(-dist^2/(2*sig2^2));
+        end
     end
-    if length( sigX ) == 1
-        sigX(2) = sigX(1);
-    end
-    if length( sigX ) == 1
-        sigX(2) = sigX(1);
-    end
-    if length( sigX ) == 1
-        sigX(2) = sigX(1);
-    end
-
+    filter3 = filter1 .* filter2;
 end
---
+++ bandpass_ui.m
@@ -0,0 +1,158 @@
+function varargout = bandpass_ui(varargin)
+% BANDPASS_UI MATLAB code for bandpass_ui.fig
+%      BANDPASS_UI, by itself, creates a new BANDPASS_UI or raises the existing
+%      singleton*.
+%
+%      H = BANDPASS_UI returns the handle to a new BANDPASS_UI or the handle to
+%      the existing singleton*.
+%
+%      BANDPASS_UI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in BANDPASS_UI.M with the given input arguments.
+%
+%      BANDPASS_UI('Property','Value',...) creates a new BANDPASS_UI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before bandpass_ui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to bandpass_ui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help bandpass_ui
+
+% Last Modified by GUIDE v2.5 13-Sep-2013 15:50:05
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @bandpass_ui_OpeningFcn, ...
+                   'gui_OutputFcn',  @bandpass_ui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before bandpass_ui is made visible.
+function bandpass_ui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to bandpass_ui (see VARARGIN)
+
+% Choose default command line output for bandpass_ui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+subplot(1,2,1)
+imagesc(varargin{1})
+colormap julio
+set(gca,'DataAspectRatio',[1 1 1]);
+handles.img = varargin{1};
+guidata(hObject, handles);
+
+% UIWAIT makes bandpass_ui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = bandpass_ui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+sig1 = str2double(get(handles.edit1, 'String'));
+sig2 = str2double(get(handles.edit2, 'String'));
+img = bandpass_img( handles.img, sig1, sig2 );
+subplot(1,2,2)
+imagesc(img)
+set(gca,'DataAspectRatio',[1 1 1]);
+
+handles.filteredImg = img;
+guidata(hObject, handles);
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+uisave( 'handles.filteredImg' );
+
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
